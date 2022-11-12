e8b4a4dad5b73e1641eab2be9ca2b8c62f4e27f9
Giorgi <killervision321@gmail.com>
2022-11-12 19:43:40 +0400

    fixed some tags, added little animation for filters

--- src/app/header/filter/filter.component.ts
+++ src/app/header/filter/filter.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faArrowCircleDown,
+  faArrowRightArrowLeft,
+  faGlassMartini,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.css'],
 })
 export class FilterComponent implements OnInit {
+  //dummy filter array
+  filterItems: any[] = [
+    faGlassMartini,
+    faArrowRightArrowLeft,
+    faArrowCircleDown,
+    faGlassMartini,
+    faArrowRightArrowLeft,
+    faArrowCircleDown,
+    faGlassMartini,
+    faArrowRightArrowLeft,
+    faArrowCircleDown,
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
