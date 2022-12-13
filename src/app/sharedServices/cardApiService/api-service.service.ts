576f30fff4ffed10c37ea4536624e64a5ac1b424
Giorgi <killervision321@gmail.com>
2022-12-13 19:32:52 +0400

    added Http services

--- src/app/sharedServices/cardApiService/api-service.service.ts
+++ src/app/sharedServices/cardApiService/api-service.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCategory():Observable<any>{
+    const categoryApi ='http://airbnb-dev.us-east-1.elasticbeanstalk.com/api/Category'
+    return this.http.get(categoryApi)
+  }
+
+  getAllHotels():Observable<any>{
+    const hotelApi ='http://airbnb-dev.us-east-1.elasticbeanstalk.com/api/Hotel'
+    return this.http.get(hotelApi)
+  }
+
+  getHotelById(id:string):Observable<any>{
+    const hotelById = `http://airbnb-dev.us-east-1.elasticbeanstalk.com/api/Hotel/${id}`
+    return this.http.get(hotelById)
+  }
+
+  filterByCategory(){
+
+  }
 }
