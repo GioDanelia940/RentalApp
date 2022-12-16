af356a3f8a1b59d534e0894b8ea98ba24015bc57
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2022-12-16 06:25:38 +0400

    added headline and image

--- src/app/view/inner-pg/inner-pg.component.ts
+++ src/app/view/inner-pg/inner-pg.component.ts
@@ -1,23 +1,35 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ApiServiceService } from 'src/app/sharedServices/cardApiService/api-service.service';
 import { CardServiceService } from '../card-service.service';
 import { Card } from '../card.model';
-
+import {
+  faPerson,
+  faArrowUpFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-inner-pg',
   templateUrl: './inner-pg.component.html',
   styleUrls: ['./inner-pg.component.css'],
 })
 export class InnerPgComponent implements OnInit {
-  card!: Card;
+  card!: any;
+  faPerson: any = faPerson;
+  faArrowUpFromBracket: any = faArrowUpFromBracket;
+  faHeart: any = faHeart;
   constructor(
     private cardService: CardServiceService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private ApiService: ApiServiceService
   ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      this.card = this.cardService.getCard(params['id']);
+      this.ApiService.getHotelById(params['id']).subscribe((result) => {
+        this.card = result;
+        console.log(this.card)
+      });
     });
   }
 }
