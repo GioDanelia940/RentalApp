a4ec8bbcddf936eddf763c2328cdd1e1001195e6
Giorgi <killervision321@gmail.com>
2022-12-17 21:52:28 +0400

    finalized category filtering
    filtering by category finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,22 +7,15 @@ import { Subject } from 'rxjs';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  testSubject :Subject<boolean> = new Subject<boolean>()
-  checkHeader:boolean = true
   constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.testSubject.subscribe(response => this.checkHeader = response)
-  }
+  ngOnInit(): void {}
   checkDisplay() {
-    if (this.router.url.substring(0,5) == '/view' && this.router.url.length == 5) {
-      this.testSubject.next(true)
+    if (this.router.url.substring(0,5) == '/view' && this.router.url.length < 7) {
       return true;
     } else if(this.router.url.substring(0,6) == '/view?') {
-      this.testSubject.next(true)
       return true;
     }else{
-      this.testSubject.next(false)
       return false
     }
   }
