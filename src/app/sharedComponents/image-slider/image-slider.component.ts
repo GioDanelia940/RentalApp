bcfb6af8fe347918c8c55b442e7ee8f14b26bb35
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2022-12-25 04:13:21 +0400

    Revert "added width as input for image-slider"
    This reverts commit 8fddc965e49464c5b66737892ff3a1948f3e64e3.

--- src/app/sharedComponents/image-slider/image-slider.component.ts
+++ src/app/sharedComponents/image-slider/image-slider.component.ts
@@ -9,7 +9,5 @@ export class ImageSliderComponent implements OnInit {
   constructor() {}
   @Input() sliderImages!: string[];
   @Input() height!: string;
-  @Input() width!: string;
-
   ngOnInit(): void {}
 }
