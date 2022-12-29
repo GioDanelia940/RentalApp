ec1fd6d0fb39bae995bae388ebeb21cbf28f58b5
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2022-12-29 18:30:34 +0400

    Update calendar.component.ts

--- src/app/sharedComponents/calendar/calendar.component.ts
+++ src/app/sharedComponents/calendar/calendar.component.ts
@@ -2,24 +2,21 @@ import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Data } from '@angular/router';
 
-
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
-  @Output () dataEvent =new EventEmitter<Data>();
-  public value!: Date [];
-  
-  constructor() { }
+  @Output() dataEvent = new EventEmitter<Data>();
+  public value!: Date[];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submitdata() {
+    this.dataEvent.emit(this.value);
+    console.log('clicked');
   }
-  
-  
-   submitdata(){
-     this.dataEvent.emit(this.value);
-   }
- 
 }
