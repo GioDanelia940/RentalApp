35e87e94a50c87e2a76b270a818b0c8563f1f8e8
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2022-12-29 17:45:47 +0400

    created order model

--- src/app/user-account/order-history/order-history.component.ts
+++ src/app/user-account/order-history/order-history.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from './order.model';
+import { ApiServiceService } from '../../sharedServices/cardApiService/api-service.service';
 @Component({
   selector: 'app-order-history',
   templateUrl: './order-history.component.html',
-  styleUrls: ['./order-history.component.css']
+  styleUrls: ['./order-history.component.css'],
 })
 export class OrderHistoryComponent implements OnInit {
-
-  constructor() { }
+  orders: Order[] = [
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+    new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
+  ];
+  constructor(private http: ApiServiceService) {}
 
   ngOnInit(): void {
-  }
 
+  }
 }
