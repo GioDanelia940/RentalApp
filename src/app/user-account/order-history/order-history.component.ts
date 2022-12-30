967bbfcba8995467a3bb7a6e65808334311d7e7a
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2022-12-30 13:28:29 +0400

    filled order-history html and added order modal

--- src/app/user-account/order-history/order-history.component.ts
+++ src/app/user-account/order-history/order-history.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from './order.model';
 import { ApiServiceService } from '../../sharedServices/cardApiService/api-service.service';
+import { MatDialog } from '@angular/material/dialog';
+import { OrderModalComponent } from './order-modal/order-modal.component';
 @Component({
   selector: 'app-order-history',
   templateUrl: './order-history.component.html',
@@ -16,7 +18,7 @@ export class OrderHistoryComponent implements OnInit {
     new Order('ef4ce06a-bb68-47bb-9c06-1e9b6e39ce8f', 2, 500),
   ];
   hotels!: any[];
-  constructor(private http: ApiServiceService) {}
+  constructor(private http: ApiServiceService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.http.getAllHotels().subscribe((response: any) => {
@@ -25,5 +27,14 @@ export class OrderHistoryComponent implements OnInit {
   }
   getHotelById(id: string) {
     return this.hotels.filter((hotel) => hotel.id == id)[0];
+  }
+  openDialogOrder(order: any, hotel: any): void {
+    const dialogRef = this.dialog.open(OrderModalComponent, {
+      width: '1000px',
+      data: { order: order, hotel: hotel },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
